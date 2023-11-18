2c41fc0466831943dae0760c7cfd7b8e36f0e637
Elkin-Rodriguez <elkinferney@soy.sena.edu.co>
2023-11-18 14:44:00 -0500

    v.1

---
+++ PAIMANWEBS/PAIMANREPOSTERIA/PAIMANREPOSTERIA/Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using PAIMANREPOSTERIA.Models;
+using System.Data.Entity;
+namespace PAIMANREPOSTERIA.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("PAIMANREPOSTERIA")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
